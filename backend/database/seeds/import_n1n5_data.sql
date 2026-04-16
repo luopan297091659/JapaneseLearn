e6236711528b2e13838151ba24062d623a55dc8a
luopan297091659 <luopan297091659@gmail.com>
2026-04-16 18:57:35 +0900

    chore: remove remaining LFS pointer files (seed data, content unavailable)

--- backend/database/seeds/import_n1n5_data.sql
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:587d8ace1d43258673bcc746095a47b9f4dde421ed1e5cab7c79b07f130ca6c1
-size 2069887
--- backend/database/seeds/import_n4_vocab.sql
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:5018a09d43e3c9a664b388775715eb5864d8f1ce6472b467396b1e0a84f5991d
-size 883734
